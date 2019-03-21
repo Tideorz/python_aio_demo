c46f2255e93803cb95113ff481232af47d0f3fc3
Ted Zhu <tzhu@mintel.com>
2019-03-21 17:28:31 +0800

    add throw and stopiteration to generator demo

--- async_web_crawler/how_generator_works.py
+++ async_web_crawler/how_generator_works.py
@@ -26,10 +26,16 @@ def gen_e():
     val_c = val_b * 10
     print(val_c)
 
+class CancelError(Exception):
+    pass
+
 def gen_f():
-    a = yield "I don't want to continue to run next line, jump out of gen_f()!!!"
-    if a == "= =":
-        yield "Fuck off!!! (jump into gen_f() again)"
+    try:
+        a = yield "I don't want to continue to run next line, jump out of gen_f()!!!"
+        if a == "= =":
+            yield "Fuck off!!! (jump into gen_f() again)"
+    except CancelError:
+        print("you throw a CancelError exception into generator")  
 
 def dis_method(f):
     dis.dis(gen_f)
@@ -39,9 +45,15 @@ if __name__ == '__main__':
         a = gen_f()
         dis_method(gen_f)
         print(next(a))
-        #print(a.gi_frame.f_lasti)
-        tell_gen_f = input('What you to tell to the method gen_f() :')
+        print(a.gi_frame.f_lasti)
+        tell_gen_f = input('What you to tell to the method gen_f(): ')
+        if tell_gen_f == 'cancel':
+            a.throw(CancelError('test'))
+            print(a.gi_frame.f_lasti)
         return_value = a.send(tell_gen_f)
+        print(a.gi_frame.f_lasti)
         print(return_value)
+        #next(a)
     except StopIteration as e:
-        print("raise StopIteration")
+        print("hit the end of generator and raise StopIteration")
+        print(a.gi_frame.f_lasti)
