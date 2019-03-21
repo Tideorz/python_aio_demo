e1111d5cb7c8068f485a6a4bd829d328b9205405
Ted Zhu <tzhu@mintel.com>
2019-03-21 16:53:32 +0800

    change generator demo code

--- async_web_crawler/how_generator_works.py
+++ async_web_crawler/how_generator_works.py
@@ -1,43 +1,47 @@
 #!/usr/env/bin python
+import dis
 
 def func_test():
-    print 'zhc'
+    print('zhc')
 
 def func_return():
     return 'zhc'
 
 def gen_a():
     val_a = yield 'zhc'
-    print val_a + '1993'
+    print(val_a + '1993')
     val_b = yield 'abc'
     yield 'test'
 
 def gen_c():
     val_a = yield func_test()
-    print val_a
+    print(val_a)
 
 def gen_d():
     val_a = yield func_return()
-    print val_a
+    print(val_a)
 
 def gen_e():
-    val_b = yield 10
-    val_c = val_b * 10
-
-def gen_f():
     val_b = yield
     val_c = val_b * 10
-    print val_c
+    print(val_c)
 
-def gen_g():
-    a = yield "I don't want to continue to run next line"
-    if a == "You must do it = =":
-        yield "Fuck off"
+def gen_f():
+    a = yield "I don't want to continue to run next line, jump out of gen_f()!!!"
+    if a == "= =":
+        yield "Fuck off!!! (jump into gen_f() again)"
+
+def dis_method(f):
+    dis.dis(gen_f)
 
 if __name__ == '__main__':
-    a = gen_a()
-    import dis
-    dis.dis(gen_a)
-    next(a)
-    print a.gi_frame.f_lasti
-    a.send('zhn')
+    try:
+        a = gen_f()
+        dis_method(gen_f)
+        print(next(a))
+        #print(a.gi_frame.f_lasti)
+        tell_gen_f = input('What you to tell to the method gen_f() :')
+        return_value = a.send(tell_gen_f)
+        print(return_value)
+    except StopIteration as e:
+        print("raise StopIteration")
